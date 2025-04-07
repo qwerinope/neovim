cfc325d7d60e180e08895f011601d0d610a29664
qwerinope <me@qwerinope.com>
2025-04-07 22:50:38 +0200

    update lockfile, improved discord-rpc

--- lua/plugins/discord-rpc.lua
+++ lua/plugins/discord-rpc.lua
@@ -2,9 +2,7 @@ return {
   'andweeb/presence.nvim',
   version = '*',
   lazy = false,
-  config = function()
-    require('presence').setup {
-      neovim_image_text = 'emacs is for normies',
-    }
-  end,
+  opts = {
+    neovim_image_text = "emacs is for normies",
+  }
 }
